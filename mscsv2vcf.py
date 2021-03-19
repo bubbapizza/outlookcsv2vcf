d8397749a2ce5583600b6eaa1775ae699492cb21
Shawn Wilson <shawn@rj11.ca>
2021-03-18 23:05:44 -0400

    Added fields that are required for vcard3

--- mscsv2vcf.py
+++ mscsv2vcf.py
@@ -101,3 +101,53 @@ USER3                       = 89
 USER4                       = 90
 WEB_PAGE                    = 100
 
+
+function CSVrow2vobject():
+
+   # Here's the columns I need to convert:
+   TITLE                       = 0
+   FIRST_NAME                  = 1
+   MIDDLE_NAME                 = 2
+   LAST_NAME                   = 3
+   SUFFIX                      = 4
+   COMPANY                     = 5
+   DEPARTMENT                  = 6
+   JOB_TITLE                   = 7
+   BUSINESS_STREET             = 8
+   BUSINESS_STREET2            = 9
+   BUSINESS_STREET3            = 10
+   BUSINESS_CITY               = 11
+   BUSINESS_STATE              = 12
+   BUSINESS_POSTAL_CODE        = 13
+   BUSINESS_COUNTRY            = 14
+   HOME_STREET                 = 15
+   HOME_STREET2                = 16
+   HOME_STREET3                = 17
+   HOME_CITY                   = 18
+   HOME_STATE                  = 19
+   HOME_POSTAL_CODE            = 20
+   HOME_COUNTRY                = 21
+   BUSINESS_FAX                = 30
+   BUSINESS_PHONE              = 31
+   BUSINESS_PHONE2             = 32
+   COMPANY_MAIN_PHONE          = 35
+   HOME_FAX                    = 36
+   HOME_PHONE                  = 37
+   HOME_PHONE2                 = 38
+   MOBILE_PHONE                = 40
+   OTHER_FAX                   = 41
+   OTHER_PHONE                 = 42
+   PAGER                       = 43
+   ASSISTANT_NAME              = 50
+   CATEGORIES                  = 54
+   EMAIL_ADDRESS               = 57
+   EMAIL_TYPE                  = 58
+   EMAIL_DISPLAY_NAME          = 59
+   EMAIL2_ADDRESS              = 60
+   EMAIL2_TYPE                 = 61
+   EMAIL2_DISPLAY_NAME         = 62
+   INITIALS                    = 70
+   NOTES                       = 77
+   OFFICE_LOCATION             = 78
+   WEB_PAGE                    = 100
+
